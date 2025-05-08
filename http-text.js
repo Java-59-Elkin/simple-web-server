14d7b63298487f77e36f40a444615ee329042364
Mikhail <elkinms85@gmail.com>
2025-05-08 22:03:25 +0300

    First commit

---
+++ http-text.js
@@ -0,0 +1,31 @@
+import http from 'http';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+//     res.end('<h1>Hello World!</h1>');
+// });
+
+// const server = http.createServer((req, res) => {
+//     res.end(req.url);
+// });
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    if (req.url === '/users') {
+        res.write('<h1>Hello User</h1>');
+    }
+    if (req.url === '/group') {
+        res.write('<h1>Hello Group</h1>');
+    }
+    res.end();
+});
+
+server.listen(port, () => {
+    console.log(`Server started on port ${port}. Press Ctrl-C to finish`);
+});
+
+
